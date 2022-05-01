a87a43d8dd7c4eb7b04db2bc903a2f3d64579774
Justin Lusby <jlusby13@gmail.com>
2022-05-01 11:32:08 -0600

    change api from recipes to movies

--- demo.py
+++ demo.py
@@ -9,42 +9,53 @@ app = Flask(__name__)
 #defualt  
 @app.route('/')
 def hello():
-    text = "Hello World!"  
+    text = "This is a movie api!"  
     return text
 
-@app.route("/recipes",methods=['GET','POST'])
+@app.route("/movies",methods=['GET','POST'])
 def get_names():
     json_url = os.path.join("data","data.json")
     #Post new recipe
     if request.method == 'POST':
-        name = request.form['name']
-        ingredients = request.form['ingredients']
-        instructions = request.form['instructions']
-        new_recipe = {  "name":name,
-                        "ingredients":ingredients,
-                        "instructions":instructions
+        title = request.form['title']
+        cast = request.form['cast']
+        tags = request.form['tags']
+        runtime = request.form['runtime']
+        format = request.form['format']
+        new_movie = {  "title":title,
+                        "cast":cast,
+                        "tags":tags,
+                        "runtime":runtime,
+                        "fornat":format
                         }
         with open(json_url, "r+") as file:
             data_json = json.load(file)
-            data_json["recipes"].append(new_recipe)
+            data_json["recipes"].append(new_movie)
             file.seek(0)
             json.dump(data_json, file)
-        return "Successfully added a recipe"
+        return "Successfully added a new movie"
         
     #Get data if not Posting
     data_json = json.load(open(json_url))
-    data = data_json["recipes"]
-    recipe_names = [x['name'] for x in data]
-    return jsonify(recipe_names)
+    data = data_json["movies"]
+    movie_names = [x['title'] for x in data]
+    return {"Movies" : movie_names}
 
-#Get recipe ingredients and number of steps
-@app.route("/recipes/details/<name>",methods=['GET'])
-def get_ingredients(name):
+#Get movie details from movie with specified title
+@app.route("/movies/details/<title>",methods=['GET'])
+def get_movies(title):
     json_url = os.path.join("data","data.json")
     data_json = json.load(open(json_url))
-    data = data_json["recipes"]
-    details = [(x['ingredients'], len(x['instructions'])) for x in data if x['name']==name]
-    return jsonify(details)
+    data = data_json["movies"]
+    for x in data:
+        if x['title'] == title: 
+            details = {
+                "cast": x.get("cast"),
+                "tags": x.get("tags"),
+                "runtime" : x.get("runtime"),
+                "format" : x.get("format")
+            }
+    return {"Details" : details}
 
 if __name__ == "__main__":
     app.run(debug=True)
